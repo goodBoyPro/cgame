75a4cc3b135d10215e7fb278d9837f5e468f6fd0
xiaoming <xiaoming@qq.com>
2025-05-11 22:30:46 +0800

    周日2025-05-11 22:30 Windows平台

--- j.js
+++ j.js
@@ -1,16 +1,16 @@
 let topicJson = null;
 let topicId = 0;
 let score = 0;
-let answer = new Array();
+let answer = "";
 let finished = new Array();
-
+let local = 0;
 class Page {
     constructor() {
         this.topic = document.getElementById("topic");
         this.box = document.getElementById("box");
         this.box2 = document.getElementById("box2");
-        this.answer = document.getElementById("answer");
-        this.score = document.getElementById("score");
+        // this.answer = document.getElementById("answer");
+        // this.score = document.getElementById("score");
         this.btnNext = document.getElementById("btnNext");
         this.btnNext.addEventListener("click", function () {
             topicId++;
@@ -22,13 +22,15 @@ class Page {
             setTopic(TCA);
         });
         this.btnReset = document.getElementById("btnReset");
-        this.btnReset.addEventListener("click",reset);
+        this.btnReset.addEventListener("click", reset);
         this.btnLook = document.getElementById("btnLook");
-        this.btnOpenfile = document.getElementById("btnOpenFile");
-        this.btnOpenfile.addEventListener("change", function (evnet) {
-            let file = evnet.target.files[0];
-            gameBeginFromBtn(file);
-        })
+        if (local) {
+            this.btnOpenfile = document.getElementById("btnOpenFile");
+            this.btnOpenfile.addEventListener("change", function (evnet) {
+                let file = evnet.target.files[0];
+                gameBeginFromBtn(file);
+            })
+        }
 
     }
     func1() { console.log('test') }
@@ -38,9 +40,13 @@ function gameBegin() {
     fetch("ti.json")
         .then(response => { return response.json(); })
         .then(file => {
-            console.log(file["all"][topicId]['topic']);
             topicJson = file;
             setTopic(topicJson["all"][topicId]);
+            page.btnNext.disabled = false;
+            page.btnReset.disabled = false;
+            page.btnLook.disabled = false;
+            page.btnNext.disabled = false;
+            page.btnNext.innerHTML = "下一题";
         })
 }
 function gameBeginFromBtn(file_) {
@@ -75,40 +81,41 @@ function setTopic(TCA) {
             let block2 = document.createElement('div');
             block2.className = "block";
             block2.textContent = v;
-            answer.push(v);            
+            answer = answer + v;
             page.box2.appendChild(block2);
             addScore();
         });
         peace.appendChild(block);
-        
+
     });
     page.box.appendChild(peace);
 }
-function reset() { page.box2.innerHTML = ''; answer = []; }
+function reset() { page.box2.innerHTML = ''; answer = ""; }
 function end() {
     page.box.innerHTML = "";
     page.box2.innerText = "完成啦~~,\n正在评分~";
     page.btnNext.disabled = true;
     page.btnNext.innerHTML = "已完成";
-    setTimeout(function () { page.box2.innerText = "你的分数是：\n";},3000);
-    
+    setTimeout(function () { page.box2.innerText =`你的分数是：\n${score}`; }, 3000);
+
 }
 function addScore() {
     if (compare(topicJson["all"][topicId]["answer"], answer)) {
         score += 10;
-        page.score.innerHTML = `分数:${score}`;
+        // page.score.innerHTML = `分数:${score}`;
         page.box2.innerHTML = '';
         let a = document.createElement('div');
         a.className = 'block';
         a.textContent = topicJson["all"][topicId]["char"];
-        
-        page.box2.appendChild(a);
-    
-    }        
-    
-}
-function compare(array1, array2) { 
 
-    return JSON.stringify(array1) === JSON.stringify(array2);
+        page.box2.appendChild(a);
+
+    }
+
 }
-// gameBegin()
+function compare(str1, str2) {
+
+    return str1 === str2;
+}
+if(!local)
+gameBegin()
